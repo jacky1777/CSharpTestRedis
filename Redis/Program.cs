8313a171e6c5840c38bcc31f7b84919abb9fa3f7
Jacky Wang <jacky1777@gmail.com>
2016-11-21 22:20:25 +0800

    init

---
+++ Redis/Program.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Redis {
+    class Program {
+        static ConnectionMultiplexer redisConn = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+        static void Main(string[] args) {
+            while (true) {
+                var data = GetKey("AAA");
+                if (data == null) {
+                    SetKey("AAA", "12345");
+                    Console.WriteLine($"Add to Cache");
+                }
+                else {
+                    Console.WriteLine($"Get From Cache");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        static string GetKey(string key) {
+            var redisDatabase = redisConn.GetDatabase();
+
+            // 取得快取資料
+            var value = redisDatabase.StringGet(key);
+            return value;
+        }
+        static void SetKey(string key, string value) {
+
+            var redisDatabase = redisConn.GetDatabase();
+            redisDatabase.StringSet(key, value, TimeSpan.FromSeconds(10));
+        }
+    }
+}
